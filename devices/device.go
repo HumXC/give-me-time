e2e981c34cfd8060c481ac982bb20750b4719af7
HumXC <Hum-XC@outlook.com>
2023-04-01 16:49:31 +0800

    refactor (engine): 删除 Screenshot，StartApp, StopApp 函数

--- devices/device.go
+++ devices/device.go
@@ -1,9 +1,6 @@
 package devices
 
 import (
-	"fmt"
-	"strings"
-
 	"github.com/HumXC/adb-helper"
 )
 
@@ -14,23 +11,4 @@ type Input interface {
 type Device struct {
 	Input Input
 	ADB   adb.ADBRunner
-}
-
-func (d *Device) Screenshot() ([]byte, error) {
-	b, err := d.ADB("shell screencap -p ")
-	if err != nil {
-		return nil, fmt.Errorf("failed to capture screenshot: %w", err)
-	}
-	return b, nil
-}
-
-func (d *Device) StartApp(packageName string) error {
-	_, err := d.ADB("shell am start " + packageName)
-	return err
-}
-
-func (d *Device) StopApp(packageName string) error {
-	pkgName, _, _ := strings.Cut(packageName, "/")
-	_, err := d.ADB("shell am force-stop " + pkgName)
-	return err
 }
